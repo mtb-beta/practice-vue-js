4b81c9bb1385afbc8d72baccde40a2e4c5c0c7f3
Tatsuya Matoba <mtb.toya0403@gmail.com>
2021-03-01 22:33:23 +0900

    親コンポーネントのリストを元にレンダリングする

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 Vue.component('comp-child', {
-  // テンプレートで受け取ったvalを使用
-  template: '<p>{{ val }}</p>',
-  // 受け取る属性名を指定
-  props: ['val']
+  template: '<li>{{ name }} HP.{{ hp }}</li>',
+  props: ['name', 'hp']
 })
 
 new Vue({
   el: '#app',
   data: {
-    valueA: 'これは子A',
-    valueB: 'これは子B'
+    list: [
+      { id: 1, name: 'スライム', hp: 100 },
+      { id: 2, name: 'ゴブリン', hp: 200 },
+      { id: 3, name: 'ドラゴン', hp: 500 }
+    ]
   }
 })
