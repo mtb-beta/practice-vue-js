d1eb8fb27e8a5fa4537a9f9c98f442e274292460
Tatsuya Matoba <mtb.toya0403@gmail.com>
2021-03-01 22:38:15 +0900

    子コンポーネントでイベントを発火させて親コンポーネントのメソッドを呼び出す

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 Vue.component('comp-child', {
-  template: '<li>{{ name }} HP.{{ hp }}</li>',
-  props: ['name', 'hp']
+  template: '<button v-on:click="handleClick">イベント発火</button>',
+  methods: {
+    // ボタンのクリックイベントのハンドラでchilds-eventを発火する
+    handleClick: function () {
+      this.$emit('childs-event')
+    }
+  }
 })
 
 new Vue({
   el: '#app',
-  data: {
-    list: [
-      { id: 1, name: 'スライム', hp: 100 },
-      { id: 2, name: 'ゴブリン', hp: 200 },
-      { id: 3, name: 'ドラゴン', hp: 500 }
-    ]
+  methods: {
+    // childs-eventが発生するとparentsMethodが呼ばれる
+    parentsMethod: function () {
+      alert('イベントをキャッチ！ ')
+    }
   }
 })
